ca15386d6e87861dd0883d343b59daef82a3b4aa
TaeHwan Park <xotj2635@naver.com>
2024-01-12 19:11:37 +0900

    first commit

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  btnColor: "green",
+};
+export const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  btnColor: "green",
+};
